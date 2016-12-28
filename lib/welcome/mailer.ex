2fd5f58541a3d1920495c82f0128d6eb4ca22a53
Paul Andrei <pbadarau@gmail.com>
2016-12-28 17:04:07 +0100

    Refactor Mailer module to be used by Bamboo

--- lib/welcome/mailer.ex
+++ lib/welcome/mailer.ex
@@ -1,33 +1,7 @@
 defmodule Welcome.Mailer do
+  use Bamboo.Mailer, otp_app: :welcome
+
   @moduledoc """
-  A module for sending emails.
-
-  These functions are used for email confirmation and password resetting.
-
-  You need to complete these functions with the email library / module of
-  your choice.
+  A module which enables Bamboo's Mailer functionality
   """
-
-  @doc """
-  An email with a confirmation link in it.
-  """
-  def ask_confirm(_email, link) do
-    confirm_url = "http://www.example.com/sessions/confirm_email?#{link}"
-    confirm_url
-  end
-
-  @doc """
-  An email with a link to reset the password.
-  """
-  def ask_reset(_email, link) do
-    confirm_url = "http://www.example.com/password_resets/edit?#{link}"
-    confirm_url
-  end
-
-  @doc """
-  An email acknowledging that the account has been successfully confirmed.
-  """
-  def receipt_confirm(email) do
-    email
-  end
 end
