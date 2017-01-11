781cc02c57d539b340f70a184effee29aac207f4
Paul Andrei <pbadarau@gmail.com>
2017-01-11 22:55:31 +0100

    Replace username and password with System.get_env

--- config/config.exs
+++ config/config.exs
@@ -18,15 +18,12 @@ config :welcome, Welcome.Endpoint,
            adapter: Phoenix.PubSub.PG2]
 
 # Configures mailer
-# change the following configuration with the configuration recommended by
-# your provider.
-# It is recommended to use environment variables for username and password.
 config :welcome, Welcome.Mailer,
   adapter: Bamboo.SMTPAdapter,
   server: "smtp.domain",
   port: 1025,
-  username: "username",
-  password: "password",
+  username: System.get_env("SMTP_USERNAME"),
+  password: System.get_env("SMTP_PASSWORD"),
   tls: :if_available, # can be `:always` or `:never`
   ssl: false, # can be `true`
   retries: 1
